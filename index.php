e410cee773283fdd922849e028caaf2f0362543a
axl-crz <acrozier15@icloud.com>
2022-10-19 18:13:57 +0200

    Add files

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Discovering Bootstrap 5.2</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Categories
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Photos</a></li>
+                                <li><a class="dropdown-item" href="#">Articles</a></li>
+                                <li><a class="dropdown-item" href="#">Projects</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <section>
+        <div class="row">
+            <h1>Journey into Bootstrap classes</h1>
+            <div class="card col-md-3" style="width: 17rem;">
+                <img src="https://i.picsum.photos/id/83/200/200.jpg?hmac=PWpSDFTveI1bSJjmrf_vnw4ipqEELicSPpDf8jb89FI" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Visit Components lake</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn bg-primary">Read more</a>
+                </div>
+            </div>
+            <div id="card2" class="card col-md-3" style="width: 17rem;">
+                <img src="https://i.picsum.photos/id/222/200/200.jpg?hmac=GngU-e1fHxK6MMBinwEkzsh8sMkjssl_vp8G6vJrb8U" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Facing Cascade Style Sheet</h5>
+                    <p class="card-text">Some quick example text.</p>
+                    <a href="#" class="btn bg-primary">Read more</a>
+                </div>
+            </div>
+            <div class="card col-md-3" style="width: 17rem;">
+                <img src="https://i.picsum.photos/id/871/200/200.jpg?hmac=RiUSuXl3prTnCTwjRX_be5hca7AJGWw75b50TxlMcFA" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Contemplate Form Inputs river</h5>
+                    <p class="card-text">Make up the bulk of the card's content.</p>
+                    <a href="#" class="btn bg-primary">Read more</a>
+                </div>
+            </div>
+            <div class="card col-md-3" style="width: 17rem;">
+                <img src="https://i.picsum.photos/id/499/200/200.jpg?hmac=6OuceXs-LrhY-PtwyqBs_bARD8YWZEj9XuZ_ZY894Wc" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Enjoy Responsive Layout land</h5>
+                    <p class="card-text">Build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn bg-primary">Read more</a>
+                </div>
+            </div>
+
+
+        </div>
+    </section>
+<section id="section2">
+    <h2>The css frameworks are very useful</h2>
+    <p>Learning them comes with practice</p>
+</section>
+<footer>
+    <p>© 2022 wilders | images <a href="https://picsum.photos/">@picsum</a></p>
+</footer class="footer bg-primary">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+
+</html>
